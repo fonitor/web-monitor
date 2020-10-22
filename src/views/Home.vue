e44760e9c9c7cf4ce0bf556a2f5220103d9fa19b
xiaolin <462441355@qq.com>
2020-10-22 17:30:13 +0800

    * 测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a href="https://www.baidu.com">百度http://www.baidu.com</a>
+    <a href="https://www.baidu1.com">百度http://www.baidu.com</a>
+    <a href="https://www.baidu2.com">百度http://www.baidu.com</a>
   </div>
 </template>
 
@@ -13,6 +16,22 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  mounted() {
+    console.log(document.getElementsByTagName('a'))
+    let aTags = document.getElementsByTagName('a')
+    aTags.forEach(item => {
+      item.addEventListener('click', (event) => {
+        event.stopPropagation()
+        event.preventDefault()
+        console.log(event)
+        console.log('测试')
+        console.log(event.srcElement.href)
+      })
+    });
+    
+  },
+  methods: {
   }
 }
 </script>
